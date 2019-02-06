40017336284bd160c17614bb8646c5f8185006eb
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2019-02-06 21:56:34 +0300

    Update TypeAlies.swift

--- b2Degiskenler/TypeAlies.swift
+++ b2Degiskenler/TypeAlies.swift
@@ -8,7 +8,7 @@ print (minSes, maxSes)
 /*
 Aciklama: Program ekran çıktısı:0 255
 
-Artık ‘UInt8’ yerine ‘sesAyar’ takma adlı veri tipini kullanabilirim. Örneğin programın ikinci satırını 
-var minSes: sesAyar = -20 şeklinde değiştirse idik aşağıdaki gibi programın hata verdiği görülecekti;
+Artık ‘UInt8’ yerine ‘sesAyar’ takma adlı veri tipini kullanabilirim. Örneğin programın 4. satırını 
+'var minSes: sesAyar = -20' şeklinde değiştirse idik aşağıdaki gibi programın hata verdiği görülecekti;
 Error: Negative integer '-20' overflows when stored into unsigned type 'sesAyar' ('UInt8')
 */
